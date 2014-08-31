b4969792bcb1fae9199141ea39f00d8fad1a2e11
Axel <at.thehotcorner@gmail.com>
2014-08-31 19:04:55 -0400

    IE fix

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,24 @@
-if (navigator.userAgent.indexOf("MSIE") > -1) {
-    document.body.classList.add("ie10");
+function detectIE() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    var trident = ua.indexOf('Trident/');
+
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    if (trident > 0) {
+        // IE 11 (or newer) => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    // other browser
+    return false;
 }
+
+if (detectIE()) document.body.classList.add('ie');
 
 $(document).ready(function() {
     $('nav').height($('nav .wrapper').outerHeight());

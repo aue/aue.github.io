38dd681cd5c190ec8abd86eb855dfd22224c04d7
atthehotcorner <atthehotcorner@users.noreply.github.com>
2016-08-28 12:50:21 -0400

    New sliding photo intro

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,4 @@
-/*function detectIE() {
-    var ua = window.navigator.userAgent;
-    var msie = ua.indexOf('MSIE ');
-    var trident = ua.indexOf('Trident/');
-
-    if (msie > 0) {
-        // IE 10 or older => return version number
-        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-    }
-
-    if (trident > 0) {
-        // IE 11 (or newer) => return version number
-        var rv = ua.indexOf('rv:');
-        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-    }
-
-    // other browser
-    return false;
-}
-
-if (detectIE()) document.body.classList.add('ie');*/
-
-$(document).ready(function() {
+/*$(document).ready(function() {
     $('nav').height($('nav ul li.logo').outerHeight());
     NavOffset = $('nav').offset().top;
 });
@@ -34,3 +12,27 @@ $(window).scroll(function() {
     if (scrolledOver && !$('nav').hasClass('fixed')) $('nav').addClass('fixed');
     else if (!scrolledOver && $('nav').hasClass('fixed')) $('nav').removeClass('fixed');
 });
+*/
+
+document.querySelector('header .photo').classList.add('ken-burns');
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build tween
+var tl = new TimelineMax().add([
+	TweenMax.to('header .photo', 0.5, {left: 0, top: 0, bottom: 0, width: '17rem', ease: Sine.easeIn}),
+	TweenMax.to('header .faux-photo.one', 0.5, {left: '17rem', top: '2rem', bottom: '2rem', width: 0, ease: Power1.easeIn}),
+	TweenMax.to('header .faux-photo.two', 0.51, {left: '17rem', top: '4rem', bottom: '4rem', width: 0, ease: Power1.easeIn})
+]);
+
+// build scene and set duration to window height
+var scene = new ScrollMagic.Scene({offset: 50, duration: '75%'})
+	.setTween(tl)
+	.on('end', function(event) {
+		var photo = document.querySelector('header .photo');
+		if (event.scrollDirection === 'REVERSE') photo.classList.add('ken-burns');
+		else photo.classList.remove('ken-burns');
+	})
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller);
